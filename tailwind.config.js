251a47dcc77048390e27877ef14db1fbc10a5c20
Partha Roy <partharoypc@gmail.com>
2021-12-23 18:23:26 +0600

    Main Structure Created

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        borderWidth: {
+            DEFAULT: '1px',
+            '0': '0',
+            '2': '2px',
+            '3': '3px',
+            '4': '4px',
+            '6': '6px',
+            '8': '8px',
+        },
+
+        extend: {
+            colors: {
+
+
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
