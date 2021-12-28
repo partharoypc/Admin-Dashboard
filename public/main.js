b52837dfe2d15272fc9669b46b4bea8309816384
Partha Roy <partharoypc@gmail.com>
2021-12-28 16:12:32 +0600

    Fully Complete View

--- public/main.js
+++ public/main.js
@@ -1,16 +1,134 @@
-<!DOCTYPE html>
-<html lang="en">
+// First  Chart 
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="output.css">
-</head>
+var ctx = document.getElementById("myChart-01").getContext("2d");
 
-<body>
+var data = {
+    labels: ["January", "February", "March", "April"],
+    datasets: [{
+            label: "Page Impressions",
+            data: [10, 20, 30, 40],
+            borderColor: "#3B82F6",
+            backgroundColor: "#FECACA",
 
-</body>
+        },
+        {
+            label: "Adsense Clicks",
+            data: [5, 15, 10, 30],
+            backgroundColor: "#36A2EB",
+            borderColor: "#3B82F6",
+            "fill": false,
+            "type": "line",
+            borderWidth: 2,
 
-</html>
+        },
+    ]
+};
+
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+
+        scales: {
+            yAxes: [{
+                ticks: {
+                    "beginAtZero": true
+
+                }
+            }]
+        }
+    }
+});
+
+
+// Second   Chart 
+var ctx = document.getElementById("myChart-02").getContext("2d");
+
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+        label: "Views",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: "#D1D5DB",
+        borderColor: "#34D399",
+        "fill": false,
+        lineTension: 0.1,
+        borderWidth: 2,
+    }, ]
+};
+
+var myBarChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+
+        scales: {
+            yAxes: [{
+                ticks: {
+
+
+                }
+            }]
+        }
+    }
+});
+
+// 3rd  Chart 
+
+var ctx = document.getElementById("myChart-03").getContext("2d");
+
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+        label: "Likes",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: ["#F3F4F6", "#FEE2E2", "#FEF3C7", "#D1FAE5", "#DBEAFE", "#E0E7FF", "#EDE9FE"],
+        borderColor: ["#6B7280", "#F87171", "#FBBF24", "#10B981", "#3B82F6", "#6366F1", "#8B5CF6"],
+        borderWidth: 2,
+    }, ]
+};
+
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+
+        scales: {
+            yAxes: [{
+                ticks: {
+
+
+                }
+            }]
+        }
+    }
+});
+
+
+
+// 4th   Chart 
+
+var xValues4 = ["P1", "P2", "P3"];
+var yValues4 = [300, 50, 100];
+var barColors4 = [
+    "#EF4444",
+    "#3B82F6",
+    "#FBBF24"
+];
+
+new Chart("myChart-04", {
+    type: "doughnut",
+    data: {
+        labels: xValues4,
+        datasets: [{
+            backgroundColor: barColors4,
+            data: yValues4
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+
+        }
+    }
+});
